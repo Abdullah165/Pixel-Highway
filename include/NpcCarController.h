8f7dd36a43dcd1755ec2a4161e6fa02704cf7369
Abdullah Abdelaty <63372032+Abdullah165@users.noreply.github.com>
2025-10-19 20:33:15 +0300

    Pass parameters by ref instead of values to avoid copies & remove the speed variable
    The speed is defined in the Game class as a world speed

--- include/NpcCarController.h
+++ include/NpcCarController.h
@@ -8,16 +8,16 @@ class NpcCarController
 {
 public:
     NpcCarController();
-    NpcCarController(Texture2D* carTexture, Vector2 position, float speed);
+    NpcCarController(Texture2D* carTexture, Vector2 position);
 
     void Draw() const;
-    void Update(Vector2 roadPosition, int roadWidth, float worldSpeed);
-    void RePosition(Vector2 roadPosition, int roadWidth);
+    void Update(const Vector2& roadPosition, int roadWidth, float worldSpeed);
+    void Respawn(const Vector2& roadPosition, int roadWidth);
     Rectangle getRect() const;
+    void SetPosition(const Vector2& position);
 private:
-    Texture2D* m_texture;
-    Vector2 m_position;
-    float m_init_speed;
+    Texture2D* m_Texture;
+    Vector2 m_Position;
 };
 
 
--- src/NpcCarController.cpp
+++ src/NpcCarController.cpp
@@ -3,33 +3,36 @@
 #include <random>
 
 NpcCarController::NpcCarController()
-    : m_texture(nullptr), m_position{0, 0}, m_init_speed(0.0f)
-{}
+    : m_Texture(nullptr), m_Position{0, 0}
+{
+}
 
-NpcCarController::NpcCarController(Texture2D* carTexture, Vector2 position, float speed)
-    : m_texture(carTexture), m_position(position), m_init_speed(speed)
-{}
+NpcCarController::NpcCarController(Texture2D* carTexture, Vector2 position)
+    : m_Texture(carTexture), m_Position(position)
+{
+}
+
 
 void NpcCarController::Draw() const
 {
-    DrawTexture(*m_texture, m_position.x, m_position.y, WHITE);
+    DrawTexture(*m_Texture, m_Position.x, m_Position.y, WHITE);
 }
 
-void NpcCarController::Update(Vector2 roadPosition, int roadWidth,float worldSpeed)
+void NpcCarController::Update(const Vector2& roadPosition, int roadWidth, float worldSpeed)
 {
-    m_position.y += worldSpeed;
+    m_Position.y += worldSpeed;
 
     // Check if the car gets the end of road.
-    if (m_position.y > GetScreenHeight() + 10.0)
+    if (m_Position.y > GetScreenHeight() + 10.0)
     {
-        RePosition(roadPosition, roadWidth);
+        Respawn(roadPosition, roadWidth);
     }
 }
 
-void NpcCarController::RePosition(Vector2 roadPosition, int roadWidth)
+void NpcCarController::Respawn(const Vector2& roadPosition, int roadWidth)
 {
     // RePosition.
-    m_position.y = -10;
+    m_Position.y = -10;
 
     const int min_value = roadPosition.x + 20; // beginning of the road
     const int max_value = roadPosition.x + roadWidth - 70; // End of the road
@@ -38,11 +41,17 @@ void NpcCarController::RePosition(Vector2 roadPosition, int roadWidth)
     std::mt19937 gen(rd()); // Mersenne Twister engine
     std::uniform_int_distribution<> dist(min_value, max_value);
 
-    m_position.x = dist(gen);
+    m_Position.x = dist(gen);
+}
+
+void NpcCarController::SetPosition(const Vector2& position)
+{
+    m_Position = position;
 }
 
 Rectangle NpcCarController::getRect() const
 {
-    return Rectangle(m_position.x + 12, m_position.y + 5, static_cast<float>(m_texture->width - 25),
-                     static_cast<float>(m_texture->height - 15));
+    return Rectangle(m_Position.x + 12, m_Position.y + 5, static_cast<float>(m_Texture->width - 25),
+                     static_cast<float>(m_Texture->height - 15));
 }
+
